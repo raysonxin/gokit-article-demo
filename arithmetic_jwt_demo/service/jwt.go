a55deee43ba6360e1a1de3b7ba2375495cdc53e2
raysonxin <xindachao@gmail.com>
2019-03-11 14:28:08 +0800

    修改注释

--- arithmetic_jwt_demo/service/jwt.go
+++ arithmetic_jwt_demo/service/jwt.go
@@ -8,6 +8,7 @@ import (
 //secret key
 var secretKey = []byte("abcd1234!@#$")
 
+// ArithmeticCustomClaims 自定义声明
 type ArithmeticCustomClaims struct {
 	UserId string `json:"userId"`
 	Name   string `json:"name"`
@@ -15,15 +16,18 @@ type ArithmeticCustomClaims struct {
 	jwt.StandardClaims
 }
 
+// jwtKeyFunc 返回密钥
 func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
 	return secretKey, nil
 }
 
+// Sign 生成token
 func Sign(name, uid string) (string, error) {
 
-	//两分钟后过期
+	//为了演示方便，设置两分钟后过期
 	expAt := time.Now().Add(time.Duration(2) * time.Minute).Unix()
 
+	// 创建声明
 	claims := ArithmeticCustomClaims{
 		UserId: uid,
 		Name:   name,
@@ -32,6 +36,10 @@ func Sign(name, uid string) (string, error) {
 			Issuer:    "system",
 		},
 	}
+
+	//创建token，指定加密算法为HS256
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	//生成token
 	return token.SignedString(secretKey)
 }
